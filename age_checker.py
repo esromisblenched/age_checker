3c115a92d3fcd5ec12400620d2407f2835fa8d9f
esromisblenched <gnanarajahe@gmail.com>
2026-06-06 17:53:46 -0400

    Add files via upload

---
+++ age_checker.py
@@ -0,0 +1,9 @@
+a = int(input("Enter your age: "))
+    
+if a < 10:
+    print("You many not enroll into the class because your small")
+else:
+    if a >= 10 and a <= 20:
+        print("You may enroll into the class")
+    else:
+        print("You are too old to enroll into the class")
